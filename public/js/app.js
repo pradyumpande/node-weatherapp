8ec1eed7a57a233e9c39f796a98dd5dd221c7dca
pradyumpande <pradhumn.pande2019@gmail.com>
2022-12-21 12:46:07 +0530

    Remove unncessarry console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-
-console.log("client side javascript is loaded")
-
-
 const weatherform = document.querySelector('form')
 const searchelement=document.querySelector('input')
 const msg1=document.querySelector('#msg1')
